2558d1c136c2e17ce499213c059f557ce97335e4
dhandiyy <dhandi.yudhit@gmail.com>
2025-03-04 06:43:45 +0700

    update home page: fix mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import SmallCard from "@/components/SmallCard";
 import Title from "@/components/Title";
 import Image from "next/image";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import kepalaDesaPict from "/public/kepala_desa/kepaladesa.jpg";
 
 export default function Home() {
 	const textDuko =
@@ -13,7 +14,7 @@ export default function Home() {
 		"Pelayanan desa tentang nikah gratis memberikan kesempatan bagi pasangan yang kurang mampu untuk melangsungkan pernikahan tanpa biaya. Program ini difasilitasi oleh pemerintah desa yang membantu dalam hal administrasi, pengurusan dokumen, serta proses pernikahan. Tujuannya adalah untuk mempermudah pasangan yang ingin menikah namun terkendala biaya, sekaligus memastikan legalitas pernikahan mereka sesuai dengan ketentuan hukum.";
 
 	return (
-		<div className="md:-mb-72">
+		<div className="md:-mb-72 -mb-36">
 			{/* SECION1 */}
 			<Hero />
 
@@ -26,8 +27,8 @@ export default function Home() {
 					/>
 				</div>
 			</div>
-			<div className="min-h-screen w-full bg-custom_background md:rounded-tl-[250px] rounded-xl mt-3 pb-72">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10 px-6 md:px-24 py-7">
+			<div className="min-h-screen w-full bg-custom_background md:rounded-tl-[250px] rounded-xl mt-3 md:pb-72 pb-36">
+				<div className="grid grid-cols-1 md:grid-cols-2 md:gap-10 px-2 md:px-24 py-4">
 					<div className="relative mt-20 md:-mt-48 flex items-center justify-center rounded-lg invisible md:visible">
 						<div className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[400px] md:h-[400px] lg:w-[600px] lg:h-[600px]">
 							<Image
@@ -41,10 +42,11 @@ export default function Home() {
 						</div>
 					</div>
 
-					<div className="space-y-4 flex flex-col">
+					{/* SECTION2 */}
+					<div className="md:space-y-4 flex flex-col gap-4">
 						<SmallCard text={textDuko} />
 
-						<div className="flex flex-col sm:flex-row gap-4">
+						<div className="flex flex-col sm:flex-row gap-4 text-sm md:text-base">
 							<div className="bg-white rounded-xl p-4 md:p-7 flex-1">
 								<p>
 									<i>Total Penduduk: </i>
@@ -64,30 +66,31 @@ export default function Home() {
 							</div>
 						</div>
 
-						<button className="bg-primary px-7 py-3 rounded-full text-white hover:bg-primary/90 duration-200 transition-colors w-1/2">
-							Profile Desa <FaLongArrowAltRight className="inline text-xl ml-3" />
+						<button className="bg-primary md:px-7 md:py-3 py-1 rounded-full text-white hover:bg-primary/90 duration-200 transition-colors w-1/2 text-sm md:text-base">
+							Profile Desa <FaLongArrowAltRight className="inline size-4 ml-3" />
 						</button>
 					</div>
 
-					<div className="relative mt-20">
-						<div className="absolute -left-3 w-full h-full bg-primary rounded-xl" />
-						<div className="relative bg-white p-7 rounded-xl flex flex-col justify-between h-full w-full z-50">
-							<div className="flex items-center">
-								<Image
-									src="/kepala_desa/kepaladesa.jpg"
-									alt="Deskripsi hero section"
-									width={50}
-									height={50}
-									className="object-cover rounded-full"
-									sizes="(max-width: 768px) 100vw, 50vw"
-									priority
-								/>
-								<p className="font-bold ml-4 text-xl">Kh.Ir.Makrif Amin</p>
+					<div className="md:relative md:mt-20">
+						<div className="absolute -left-3 w-full h-full bg-primary rounded-xl hidden md:block" />
+
+						{/* HIDDENN */}
+						<div className="relative bg-white p-7 rounded-xl md:flex md:flex-col md:justify-between h-full md:w-full z-50 hidden">
+							<div className="flex md:items-center">
+								<div className="h-16 w-16">
+									<Image
+										src={kepalaDesaPict}
+										alt="Deskripsi hero section"
+										className="object-cover rounded-full"
+										priority
+									/>
+								</div>
+								<p className="font-bold md:ml-4 md:text-xl text-sm">Kh.Ir.Makrif Amin</p>
 							</div>
+
 							<div>
-								<p className="mt-16">
-									Selamat datang di Desa Duko. Saya sebagai kepala desa berjanji akan memakmurkan desa, meningkatkan infrastruktur,
-									kesehatan dan pendidikan
+								<p className="mt-6 md:mt-16 text-sm md:text-base">
+									Selamat datang di Desa Duko. Saya sebagai kepala desa berjanji akan memakmurkan desa, meningkatkan infrastruktur, kesehatan dan pendidikan
 								</p>
 								<div className="flex items-center mt-4">
 									<div className="h-2 w-2 bg-primary" />
@@ -97,31 +100,31 @@ export default function Home() {
 								</div>
 							</div>
 						</div>
-					</div>
+					</div> 
 
 					<div className="relative mt-20">
 						<div className="flex flex-col">
 							<div className="flex gap-4">
 								<div className="bg-white rounded-xl p-7 flex flex-col flex-1">
 									<div className="flex flex-1 items-center justify-center">
-										<div className={`md:w-[6px] w-[8px] h-9 bg-primary rounded-full mr-5`} />
-										<p className="text-xl my-auto flex-1">Jumlah Program Bansos</p>
+										<div className={`md:w-[6px] w-[5px] self-stretch bg-primary rounded-full mr-5`} />
+										<p className="md:text-xl my-auto flex-1 text-sm">Jumlah Program Bansos</p>
 									</div>
 
-									<p className="font-montserrat text-9xl font-semibold mt-16">88</p>
+									<p className="font-montserrat md:text-9xl font-bold mt-16 text-2xl">88</p>
 								</div>
 
 								<div className="bg-white rounded-xl p-7 flex flex-col flex-1">
 									<div className="flex flex-1 items-center justify-center">
-										<div className={`md:w-[6px] w-[8px] h-14 bg-primary rounded-full mr-5`} />
-										<p className="text-xl my-auto flex-1">Jumlah Penduduk Penerima Bansos</p>
+										<div className={`md:w-[6px] w-[5px] self-stretch bg-primary rounded-full mr-5`} />
+										<p className="md:text-xl my-auto flex-1 text-sm">Jumlah Penduduk Penerima Bansos</p>
 									</div>
 
-									<p className="font-montserrat text-9xl font-semibold mt-16">88</p>
+									<p className="font-montserrat md:text-9xl font-bold mt-16 text-2xl">88</p>
 								</div>
 							</div>
-							<button className="bg-primary px-7 py-3 mt-4 rounded-full text-white hover:bg-primary/90 transition-colors">
-								Cek Penerima Bansos <FaLongArrowAltRight className="inline text-xl ml-3" />
+							<button className="bg-primary py-1 mt-4 rounded-full text-white hover:bg-primary/90 transition-colors text-sm md:text-base">
+								Cek Penerima Bansos <FaLongArrowAltRight className="inline size-4 ml-3" />
 							</button>
 						</div>
 					</div>
@@ -135,17 +138,11 @@ export default function Home() {
 						/>
 
 						<div className="border border-white flex p-4 rounded-[45px] gap-4 mt-6 md:flex-row flex-col">
-							<div className="bg-white p-4 rounded-[38px] items-center">
+							<div className="bg-white p-4 rounded-[38px] items-center text-sm md:text-base">
 								<ul>
-									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">
-										Nikah Gratis
-									</li>
-									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">
-										Sunnat Massal
-									</li>
-									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">
-										Pengajian Akbar
-									</li>
+									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">Nikah Gratis</li>
+									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">Sunnat Massal</li>
+									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">Pengajian Akbar</li>
 									<li className="border border-gray-200 p-2 rounded-3xl hover:cursor-pointer hover:bg-secondary active:bg-primary active:text-white my-1">
 										Surat Keterangan Tidak Mampu
 									</li>
